e6cf93af4b0ccc019ee054f6b5110da02a04eab8
luismartinez205 <luisfrancisco198@gmail.com>
2025-01-04 22:55:54 -0500

    Update Galeria component with new image sources and adjust ImageGallery props

--- src/Galeria.jsx
+++ src/Galeria.jsx
@@ -6,17 +6,25 @@ import Header from './Header';
 
 const images = [
     {
-      original: "https://picsum.photos/id/1018/1000/600/",
+      original: "https://mediaim.expedia.com/destination/1/25f99a393b969b3735c64d4b492225c8.jpg",
       thumbnail: "https://picsum.photos/id/1018/250/150/",
     },
     {
-      original: "https://picsum.photos/id/1015/1000/600/",
+      original: "https://mediaim.expedia.com/destination/1/cbb3e7a782f03bcb2f81dbe0334a1bf4.jpg",
       thumbnail: "https://picsum.photos/id/1015/250/150/",
     },
     {
-      original: "https://picsum.photos/id/1019/1000/600/",
+      original: "https://mediaim.expedia.com/destination/1/0818460612fe223f06b5793c2827f835.jpg",
       thumbnail: "https://picsum.photos/id/1019/250/150/",
     },
+    {   original: "https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_800,q_75,w_1200/v1/clients/panama/temp_4fbd3a6c-4236-433a-94af-40ee863d741c.jpg",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+      },
+      {
+        original: "https://www.visitcentroamerica.com/wp-content/uploads/2017/10/ver-centroamerica-panama-archipielago-boca-toro-isla-colon-03.jpg",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+      },
+    
   ];
   
   
@@ -28,7 +36,7 @@ const Galeria = () => {
     <Header/>
     <div>
       <h1 className='galery-title'>Galería Imagenes De Bocas del Toro</h1>
-       <ImageGallery items={images} thumbnailPosition="left"/> 
+       <ImageGallery items={images}/> 
            
     </div>
     
